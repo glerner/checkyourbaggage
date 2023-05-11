ca65940151ceb028b7eee93ead28f53819eb6c0e
George Lerner <lernerconsult@gmail.com>
2023-05-10 18:39:19 -0700

    add functions.php so styles.css gets loaded

---
+++ functions.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package FSE
+ * @since 1.0.0
+ */
+
+/**
+ * Enqueue the style.css file.
+ *
+ * @since 1.0.0
+ */
+function fse_styles() {
+	wp_enqueue_style(
+		'fse-style',
+		get_stylesheet_uri(),
+		array(),
+		wp_get_theme()->get( 'Version' )
+	);
+}
+add_action( 'wp_enqueue_scripts', 'fse_styles' );
